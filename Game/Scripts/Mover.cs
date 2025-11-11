1151a48b400eab7df3223eec18f5434c41602d34
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2025-11-11 23:25:10 +0100

    setup script field serialization test case

--- Game/Scripts/Mover.cs
+++ Game/Scripts/Mover.cs
@@ -3,8 +3,10 @@ using System.Numerics;
 
 public class Mover : Component
 {
+    [Show("move")] public bool move;
+
     public override void Update(float deltaTime)
     {
-        gameObject.transform.localPosition += new Vector3(0, 0, deltaTime * -1);
+        if (move) gameObject.transform.localPosition += new Vector3(0, 0, deltaTime * -1);
     }
 }
