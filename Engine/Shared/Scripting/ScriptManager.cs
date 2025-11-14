902dff8e73cea2238d92e81a8cfa114a4030cbb6
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2025-11-14 19:57:32 +0100

    combine script compiler and manager

--- Engine/Shared/Scripting/ScriptManager.cs
+++ Engine/Shared/Scripting/ScriptManager.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using System.Text.RegularExpressions;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace Concrete;
 
@@ -30,7 +31,7 @@ public static class ScriptManager
 
         Debug.Log($"Compiling {scriptPaths.Count} script(s)...");
 
-        var compiledAssembly = ScriptCompiler.CompileScripts(scriptPaths, out var errors, out var dllbytes);
+        var compiledAssembly = CompileScriptsToAssembly(scriptPaths, out var errors, out var dllbytes);
 
         if (compiledAssembly == null)
         {
@@ -45,6 +46,65 @@ public static class ScriptManager
         Debug.Log("Scripts compiled and loaded successfully.");
 
         return dllbytes;
+    }
+
+    public static Assembly CompileScriptsToAssembly(List<string> paths, out List<Diagnostic> errors, out byte[] dllbytes)
+    {
+        dllbytes = null;
+        errors = null;
+        
+        // parse scripts into syntax trees
+        List<SyntaxTree> syntaxTrees = [];
+        for (int i = 0; i < paths.Count; i++)
+        {
+            string path = paths[i];
+            string source = File.ReadAllText(path);
+            var syntaxTree = CSharpSyntaxTree.ParseText(source);
+            syntaxTrees.Add(syntaxTree);
+        }
+
+        // get reference to the public api assembly
+        string executableDirectory = AppContext.BaseDirectory;
+        string sharedAssemblyPath = Path.Combine(executableDirectory, "Shared.dll");
+        if (!File.Exists(sharedAssemblyPath))
+        {
+            Debug.Log($"Error: Shared.dll not found at '{sharedAssemblyPath}'");
+            return null;
+        }
+        var sharedAssemblyReference = MetadataReference.CreateFromFile(sharedAssemblyPath);
+
+        // get references to dotnet runtime
+        string[] trustedPlatformAssembliesPaths = ((string)AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES")).Split(Path.PathSeparator);
+        var dotnetRuntimeReferences = trustedPlatformAssembliesPaths.Select(path => MetadataReference.CreateFromFile(path)).ToList();
+        
+        // combine all references
+        List<MetadataReference> references = [];
+        references.Add(sharedAssemblyReference);
+        references.AddRange(dotnetRuntimeReferences);
+
+        // compile scripts
+        var compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+        var compilation = CSharpCompilation.Create("ScriptsAssembly", syntaxTrees, references, compilationOptions);
+
+        // load il into memory
+        using var memoryStream = new MemoryStream();
+        var result = compilation.Emit(memoryStream);
+
+        // check for compilation errors
+        if (!result.Success)
+        {
+            errors = result.Diagnostics.Where(diagnosis => diagnosis.Severity == DiagnosticSeverity.Error).ToList();
+            return null;
+        }
+
+        // return dll bytes
+        dllbytes = memoryStream.ToArray();
+
+        // load assembly from il in memory
+        var assembly = Assembly.Load(memoryStream.ToArray());
+
+        // return the assembly
+        return assembly;
     }
 
     public static Type GetClassTypeOfScript(string scriptPath)
